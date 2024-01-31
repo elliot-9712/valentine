d177afe03ed5ad6249b7ff5f51abf4184ed3fb37
Elliot <119295503+elliot-9712@users.noreply.github.com>
2024-01-31 15:23:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function showMessage(response) {
     noButton.style.position = "absolute";
 
     // Change image source to "gun.gif"
-    document.getElementsByClassName("image")[0].src = "images/gun.gif";
+    document.getElementsByClassName("image")[0].src = "gun.gif";
 
     // Generate random coordinates within the visible container
     const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
@@ -37,7 +37,7 @@ function showMessage(response) {
     yesMessage.textContent = "LESGOOO see you on the 14th babygirl😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "dance.gif";
 
     // Remove yes button
     document.getElementById("yesButton").remove();
